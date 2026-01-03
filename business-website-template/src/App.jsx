176db5c2eb750e4740d1bfa1254e2e5aa98f9f20
Mike Pascua <projectarenadev01@gmail.com>
2026-01-03 13:16:04 +0800

    Update App.jsx

--- business-website-template/src/App.jsx
+++ business-website-template/src/App.jsx
@@ -13,32 +13,37 @@ import {
 } from './components'
 import styles from './style'
 
+// Import HelmetProvider from react-helmet-async
+import { HelmetProvider } from 'react-helmet-async'
+
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Navbar/>
+    <HelmetProvider>
+      <div className='bg-primary w-full overflow-hidden'>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar/>
+          </div>
+        </div>
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero/>      
+          </div>
+        </div>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats/>
+            <Business/>
+            <Billing/>
+            <CardDeal/>
+            <Testimonials/>
+            <Clients/>
+            <CTA/>
+            <Footer/>     
+          </div>
         </div>
       </div>
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Hero/>      
-        </div>
-      </div>
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Stats/>
-          <Business/>
-          <Billing/>
-          <CardDeal/>
-          <Testimonials/>
-          <Clients/>
-          <CTA/>
-          <Footer/>     
-        </div>
-      </div>
-    </div>
+    </HelmetProvider>
   )
 }
 
